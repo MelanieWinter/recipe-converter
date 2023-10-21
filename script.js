7344cda48b665a14f688a1eac4c232089ff8ccc5
Melanie Winter <melaniebucher830@gmail.com>
2023-10-21 02:00:56 -0700

    Adds functionality to keep formatting in pasted recipe

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const conversionInput = document.getElementById('conversion');
 const originalRecipeInput = document.getElementById('original');
 const formattedRecipeInput = document.getElementById('formatted');
@@ -7,99 +6,43 @@ function convertIngredientToDecimal(ingredient, conversionFactor) {
     // Regular expression to match fractions, mixed numbers, and whole numbers
     const ingredientRegex = /(\d+\s*\.*\d*\s*\/\s*\d+|\d+\.\d+|\d+)/g;
 
-    return ingredient.replace(ingredientRegex, function(match) {
-        if (match.includes('/') || match.includes(' ')) {
-            // If the match is a fraction or mixed number, convert it to a decimal
-            let parts = match.split(' ');
-            let decimal = 0;
+    return ingredient.replace(/(<[^>]+>)([^<]+)(<\/[^>]+>)/g, function(match, openTag, content, closeTag) {
+        const convertedContent = content.replace(ingredientRegex, function(match) {
+            if (match.includes('/') || match.includes(' ')) {
+                let parts = match.split(' ');
+                let decimal = 0;
 
-            for (const part of parts) {
-                const fractionParts = part.split('/');
-                if (fractionParts.length === 2) {
-                    const numerator = parseFloat(fractionParts[0]);
-                    const denominator = parseFloat(fractionParts[1]);
-                    decimal += numerator / denominator;
-                } else {
-                    decimal += parseFloat(part);
+                for (const part of parts) {
+                    const fractionParts = part.split('/');
+                    if (fractionParts.length === 2) {
+                        const numerator = parseFloat(fractionParts[0]);
+                        const denominator = parseFloat(fractionParts[1]);
+                        decimal += numerator / denominator;
+                    } else {
+                        decimal += parseFloat(part);
+                    }
+                }
+                return `<span style="font-size: 1em;">${(decimal * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, '')}</span>`;
+            } else {
+                const number = parseFloat(match);
+                if (!isNaN(number)) {
+                    return `<span style="font-size: 1em;">${(number * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, '')}</span>`;
                 }
             }
-            return (decimal * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, ''); 
-            // Multiply by the conversion factor and remove trailing zeros
-        } else {
-            // If it's not a fraction or mixed number, try to parse it as a float
-            const number = parseFloat(match);
-            if (!isNaN(number)) {
-                return (number * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, ''); 
-                // Multiply by the conversion factor and remove trailing zeros
-            }
-        }
-        return match;
+            return match;
+        });
+
+        return openTag + convertedContent + closeTag;
     });
 }
 
-// Add an event listener to the "Convert" button
-document.querySelector('#original-recipe-div input[type="submit"]').addEventListener('click', function() {
-    // Get the conversion factor from the input
+
+document.getElementById('convert').addEventListener('click', function() {
     const conversionFactor = parseFloat(conversionInput.value);
+    const originalRecipeHTML = originalRecipeInput.innerHTML;
 
-    // Get the original recipe from the input
-    const originalRecipe = originalRecipeInput.value;
+    // Replace only the numbers within the text content with the converted numbers
+    const convertedRecipeHTML = convertIngredientToDecimal(originalRecipeHTML, conversionFactor);
 
-    // Check if the input is a valid number and if there's a recipe
-    if (!isNaN(conversionFactor) && originalRecipe) {
-        // Split the recipe into lines (assuming each line is a step)
-        const recipeLines = originalRecipe.split('\n');
-        const convertedRecipe = [];
-        for (const line of recipeLines) {
-            const convertedLine = convertIngredientToDecimal(line, conversionFactor);
-            convertedRecipe.push(convertedLine);
-        }
-        formattedRecipeInput.value = convertedRecipe.join('\n');
-    }
-    else {
-        formattedRecipeInput.value = "Please enter a valid conversion factor and recipe.";
-    }
+    formattedRecipeInput.innerHTML = convertedRecipeHTML;
 });
-
-document.querySelector('#formatted-recipe-div input[type="submit"]').addEventListener('click', function() {
-    // Handle saving the formatted recipe here
-});
-
-// const request = require('request-promise');
-// const cheerio = require('cheerio');
-
-// const url = 'https://www.thechunkychef.com/chicken-and-rice-soup/';
-// request(url)
-//   .then((html) => {
-//     const $ = cheerio.load(html);
-
-//     let recipeData = {
-//       title: $('h1.entry-title').text().trim(),
-//       prepTime: $('span.wprm-recipe-prep-time').text().trim(),
-//       cookTime: $('span.wprm-recipe-cook-time').text().trim(),
-//       totalTime: $('span.wprm-recipe-total-time').text().trim(),
-//       servings: $('span.wprm-recipe-servings').text().trim(),
-//       calories: $('span.wprm-recipe-calories').text().trim(),
-//     };
-
-//     let ingredients = [];
-//     $('ul.wprm-recipe-ingredients li').each((index, element) => {
-//       ingredients.push($(element).text().trim());
-//     });
-
-//     let instructions = [];
-//     $('div.wprm-recipe-instruction-text').each((index, element) => {
-//       instructions.push(`${index + 1}. ${$(element).text().trim()}`);
-//     });
-
-//     recipeData.ingredients = ingredients;
-//     recipeData.instructions = instructions;
-
-//     // Log the recipe data
-//     console.log(recipeData);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-
