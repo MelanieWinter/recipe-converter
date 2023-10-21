2c53fac5c35ed8ff9245cd1821b86ed9680b9072
Melanie Winter <melaniebucher830@gmail.com>
2023-10-21 03:12:39 -0700

    Adds functionality to only convert numbers followed by a measurement term afterward

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const originalRecipeInput = document.getElementById('original');
 const formattedRecipeInput = document.getElementById('formatted');
 
 function convertIngredientToDecimal(ingredient, conversionFactor) {
+    const measurementTerms = ["cup", "c", "teaspoon", "t", "tsp", "tablespoon", "T", "tbsp", "quart", "qt", "pint", "pt", "ounce", 'oz', "gallon", "gal", "lb", "lb.", "pound","milligram", "mg", "gram", "kilogram", "kg", "milliliter", "ml", "liter", "l"];
+
     // Regular expression to match fractions, mixed numbers, and whole numbers
     const ingredientRegex = /(\d+\s*\.*\d*\s*\/\s*\d+|\d+\.\d+|\d+)/g;
 
@@ -23,10 +25,18 @@ function convertIngredientToDecimal(ingredient, conversionFactor) {
                     }
                 }
                 return `<span style="font-size: 1em;">${(decimal * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, '')}</span>`;
-            } else {
+            } else if (!isNaN(parseFloat(match))) {
+                // Handle whole numbers here
                 const number = parseFloat(match);
-                if (!isNaN(number)) {
-                    return `<span style="font-size: 1em;">${(number * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, '')}</span>`;
+                return `<span style="font-size: 1em;">${(number * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, '')}</span>`;
+            } else {
+                // Check if the next word is a measurement term
+                const nextWord = content.substring(content.indexOf(match) + match.length).split(' ')[0];
+                if (measurementTerms.includes(nextWord.toLowerCase())) {
+                    const number = parseFloat(match);
+                    if (!isNaN(number)) {
+                        return `<span style="font-size: 1em;">${(number * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, '')}</span>`;
+                    }
                 }
             }
             return match;
@@ -35,6 +45,11 @@ function convertIngredientToDecimal(ingredient, conversionFactor) {
         return openTag + convertedContent + closeTag;
     });
 }
+
+
+
+
+
 
 
 document.getElementById('convert').addEventListener('click', function() {
