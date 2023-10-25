0c6a1b77d1dbba469d167e5b5c2786b744e85133
Melanie Winter <melaniebucher830@gmail.com>
2023-10-24 23:42:30 -0700

    simplifies code

--- script.js
+++ script.js
@@ -13,13 +13,11 @@ function convertRecipe() {
 document.getElementById('convert').addEventListener('click', convertRecipe);
 
 function convertIngredientToDecimal(ingredient, conversionFactor) {
-    const measurementTerms = ["cup", "c", "teaspoon", "t", "tsp", "tablespoon", "T", "tbsp", "quart", "qt", "pint", "pt", "ounce", 'oz', "gallon", "gal", "lb", "lb.", "pound","milligram", "mg", "gram", "kilogram", "kg", "milliliter", "ml", "liter", "l"];
-
     // Regular expression to match fractions, mixed numbers, and whole numbers
     const ingredientRegex = /(\d+\s*\.*\d*\s*\/\s*\d+|\d+\.\d+|\d+)/g;
 
-    return ingredient.replace(/(<[^>]+>)([^<]+)(<\/[^>]+>)/g, function(match, openTag, content, closeTag) {
-        const convertedContent = content.replace(ingredientRegex, function(match) {
+    return ingredient.replace(/(<[^>]+>)([^<]+)(<\/[^>]+>)/g, function (match, openTag, content, closeTag) {
+        const convertedContent = content.replace(ingredientRegex, function (match) {
             if (match.includes('/') || match.includes(' ')) {
                 let parts = match.split(' ');
                 let decimal = 0;
@@ -40,32 +38,11 @@ function convertIngredientToDecimal(ingredient, conversionFactor) {
                 const number = parseFloat(match);
                 return `<span style="font-size: 1em;">${(number * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, '')}</span>`;
             } else {
-                // Check if the next word is a measurement term
-                const nextWord = content.substring(content.indexOf(match) + match.length).split(' ')[0];
-                if (measurementTerms.includes(nextWord.toLowerCase())) {
-                    const number = parseFloat(match);
-                    if (!isNaN(number)) {
-                        return `<span style="font-size: 1em;">${(number * conversionFactor).toFixed(2).replace(/\.0{1,2}$/, '')}</span>`;
-                    }
-                }
+                return match; // No conversion for non-numeric content
             }
-            return match;
         });
 
         return openTag + convertedContent + closeTag;
     });
 }
-
-document.getElementById('save').addEventListener('click', function() {
-    const formattedRecipeHTML = formattedRecipeInput.innerHTML;
-
-    // Create a new jsPDF instance
-    const pdf = new jsPDF();
-
-    // Add the content (formatted recipe) to the PDF
-    pdf.fromHTML(formattedRecipeHTML, 15, 15);
-
-    // Save the PDF to a file
-    pdf.save('converted_recipe.pdf');
-});
 
